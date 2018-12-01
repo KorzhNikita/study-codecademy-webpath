42aa295d00d51068c10f70f8b6578178c41d4e82
Nikita <korzh.nikita01@gmail.com>
2018-12-01 20:23:05 +0200

    Add files via upload

---
+++ React/Create Project & Components/src/util/Yelp.js
@@ -0,0 +1,36 @@
+const apiKey = 'JXuSSe5oRDZBCLQJZ94hrvqONbT11u4biKWo7d0ic1awmRjdO45UTffki-gDrzmOXh-2WNSQFLR6V2bF3yVgxXHVvYXqwMQuthEWTK-5bDyDJmqKw7q6yILLC-juW3Yx';
+
+const Yelp = {
+	search(term, location, sortBy) {
+		return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, 
+		{ 
+			headers: {
+				Authorization: `Bearer ${apiKey}` 
+			}
+		}).then(response => {
+			return response.json();
+		}).then(jsonResponse => {
+			if (jsonResponse.businesses) {
+				console.log(jsonResponse.businesses);
+				return jsonResponse.businesses.map(business => {
+					return {
+						id: business.id,
+						imageSrc: business.image_url,
+						name: business.name,
+						address: business.location.address1,
+						city: business.location.city,
+						state: business.location.state,
+						zipCode: business.location.zip_code,
+						category: business.categories[0].title,
+						rating: business.rating,
+						reviewCount: business.review_count
+					}
+				})
+			} else {
+				console.log('error');
+			}
+		})
+	}
+}
+
+export default Yelp;
